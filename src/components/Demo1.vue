1495484289c07b77c07e7e2d1ea92224f245f9fa
dudor <dudor@qq.com>
2017-11-10 14:51:39 +0800

    add vuex

---
+++ src/components/Demo1.vue
@@ -0,0 +1,33 @@
+<template>
+  <button @click="clickMe">clickMe</button>  
+</template>
+
+<script>
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment (state) {
+      state.count++
+    }
+  }
+})
+
+export default {
+  name: 'demo1',
+  data () {
+    return {}
+  },
+  methods: {
+    clickMe () {
+      store.commit('increment')
+      console.log(store.state.count)
+    }
+  }
+
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import Demo1 from '@/components/Demo1'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: HelloWorld
+      name: 'Demo1',
+      component: Demo1
     }
   ]
 })
