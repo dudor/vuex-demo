a6280eb80e9f1af94413623fd86976701c71a360
dudor <dudor@qq.com>
2017-11-24 07:57:35 +0800

    test element-ui

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -5,11 +5,18 @@
     <button @click="decrement">-</button>
     <button @click="incrementIfOdd">IncrementIfOdd</button>
     <button @click="incrementAsync">IncrementAsync</button>
+    <el-button @click="visible = true">按钮</el-button>
+     <el-dialog :visible.sync="visible" title="Hello world">
+      <p>欢迎使用 Element</p>
+    </el-dialog>
   </div>  
 </template>
 <script>
 import {mapGetters, mapActions} from 'vuex'
 export default {
+  data: function () {
+    return { visible: false }
+  },
   computed: mapGetters('counter', ['count']),
   methods: mapActions('counter', [
     'increment',
--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
-Vue.use(ElementUI)
+Vue.use(ElementUI, { size: 'small' })
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
